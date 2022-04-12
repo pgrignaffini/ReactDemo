1ce42ab660255c87edfa7c9e1d3abf9355995879
grignaffini <grignaffinipaolo@gmail.com>
2022-04-12 11:15:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import CounterClass from "./CounterClass";
+import Counterhooks from "./CounterHooks";
+
+export const ThemeContext = React.createContext()
+
+function App() {
+  const [theme, setTheme] = useState('red')
+  return (
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      Counter
+      <CounterClass initialCount={0}></CounterClass>
+      Counter hooks
+      <Counterhooks initialCount={0}></Counterhooks>
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Toggle theme</button>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App;
